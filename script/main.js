f0ff659dfb8d2181696de74ff5590507bfa43c60
Yury Medvedev <pq39kmk@gmail.com>
2020-09-30 12:12:41 +0200

    day 1 + home

--- script/main.js
+++ script/main.js
@@ -1,12 +1,20 @@
 //day 1
+'use strict'
+
 
 const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
-const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
 const catalogList = document.querySelector('.catalog-list');
 const subCatalog = document.querySelector('.subcatalog');
-const subcatalogHeader = document.querySelector('.subcatalog-header')
+const subcatalogHeader = document.querySelector('.subcatalog-header');
+const btnReturn = document.querySelector('.btn-return');
+
+
+const overlay = document.createElement('div');
+
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
 
 const openMenu = () => {
     catalog.classList.add('open');
@@ -16,12 +24,12 @@ const openMenu = () => {
 const closeMenu = () => {
     catalog.classList.remove('open');
     overlay.classList.remove('active');
+    closeSubMenu();
 };
 
 const openSubMemu = (event) => {
     event.preventDefault();
-    const target = event.target;
-    const itemList = target.closest('.catalog-list__item');
+    const itemList = event.target.closest('.catalog-list__item');
     if (itemList) {
         subcatalogHeader.innerHTML = itemList.innerHTML;
         subCatalog.classList.add('subopen');
@@ -29,11 +37,15 @@ const openSubMemu = (event) => {
     };
 };
 
+const closeSubMenu = () => {
+    subCatalog.classList.remove('subopen');
+}
 
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 catalogList.addEventListener('click', openSubMemu);
+btnReturn.addEventListener('click', closeSubMenu);
 
 
 
